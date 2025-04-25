6f6d8ac77c0a79d63c0466d3f82bd9b866f2f45a
TUSHAR CHOUDHARY <108614136+TUSHAR91316@users.noreply.github.com>
2025-04-25 17:23:46 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DoctorsListingPage from './pages/DoctorsListingPage';
+
+function App() {
+  return (
+    <div className="bg-gray-50">
+      <DoctorsListingPage />
+    </div>
+  );
+}
+
+export default App;
